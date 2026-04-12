19d80b559442c460d6b13d5a293adf9bfcf1ab15
Bitu Lohar <bitulohar5@gmail.com>
2026-04-12 19:10:28 +0530

    Time: 7 ms (66.22%), Space: 30.2 MB (44.43%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,32 @@
+class Solution:
+    def reorderList(self, head):
+        if not head or not head.next:
+            return
+        
+        # Step 1: Find middle
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Step 2: Reverse second half
+        prev, curr = None, slow.next
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        slow.next = None  # break list
+        
+        # Step 3: Merge two halves
+        first, second = head, prev
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+            
+            first.next = second
+            second.next = tmp1
+            
+            first = tmp1
+            second = tmp2
