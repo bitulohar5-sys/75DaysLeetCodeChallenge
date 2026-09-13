37f8021e7c44ff0937b6081406b9a6a4cc83842b
Bitu Lohar <bitulohar5@gmail.com>
2026-09-13 20:07:28 +0530

    Time: 0 ms (100%), Space: 43.6 MB (92.1%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -1,14 +1,14 @@
 class Solution {
     public ListNode partition(ListNode head, int x) {
 
-        // Dummy nodes for two separate lists
+        
         ListNode lessDummy = new ListNode(0);
         ListNode greaterDummy = new ListNode(0);
 
         ListNode less = lessDummy;
         ListNode greater = greaterDummy;
 
-        // Traverse the original list
+        
         while (head != null) {
 
             if (head.val < x) {
@@ -22,7 +22,7 @@ class Solution {
             head = head.next;
         }
 
-        // Connect the two lists
+    
         greater.next = null;
         less.next = greaterDummy.next;
 
